128bf752fbcbc3c768a9d00b92f714aa200aa319
Jordan <jordansampson23@gmail.com>
2023-03-15 14:19:59 -0500

    Styling & Info

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ function Header(props) {
 
   return (
     <header>
-        <h1>Bikini Bottom Memory Game</h1>
+        <div>
+            <h1>Bikini Bottom Memory Game</h1>
+            <p className='info'>Gain points by clicking on a character but don't click the same one twice or the game is over!</p>
+        </div>
         <div>
             <h2>Score: {props.score}</h2>
             <h2>Best Score: {props.bestScore}</h2>
